895c5fe4f7c37d90b1d56a9875229a8da85e6b88
Bex Green <rebgreen18@gmail.com>
2025-02-18 13:22:08 -0500

    Create and style weather-card

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Header from "../Header/Header"
+import { useState } from 'react';
+import './App.css';
+import Header from "../Header/Header";
+import Main from '../Main/Main';
+
 
 function App() {
 
   return (
     <>
       <div className="page">
-        <div className="page__content"><Header /></div>
+        <div className="page__content">
+          <Header />
+          <Main />
+        </div>
       </div>
     </>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,20 @@
 import "./Header.css"
 import logo from "../../assets/logo-wtwr.svg"
+import avatar from  "../../assets/avatar.png"
 
 function Header() {
     return (<header className="header">
         {/* LOGO */}
         <img src={logo} alt="WTWR logo " className="header__logo" />
         {/* DATE AND PLACE */}
-        <p className="header__date-and-loc">DATE, LOCATION</p>
+        <p className="header__date-and-loc">Date, Location</p>
         {/* BUTTON(add clothes) */}
         <button className="header__add-btn">+ Add Clothes</button>
         {/* USERNAME */}
         {/* USER AVATAR */}
         <div className="header__user-container">
             <p className="header__user-title">First Last</p>
-            <img src="" alt="" className="header__user-avatar" />
+            <img src={avatar} alt="avatar" className="header__user-avatar" />
         </div>
        
         </header>)
