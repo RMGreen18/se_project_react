370d64cab4880dbb4f9ead3b893a43ad355494c5
Bex Green <rebgreen18@gmail.com>
2025-02-18 00:15:39 -0500

    Create basic structure for header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import "./Header.css"
+import logo from "../../assets/logo-wtwr.svg"
+
+function Header() {
+    return (<header className="header">
+        {/* LOGO */}
+        <img src={logo} alt="WTWR logo " className="header__logo" />
+        {/* DATE AND PLACE */}
+        <p className="header__date-and-loc">DATE, LOCATION</p>
+        {/* BUTTON(add clothes) */}
+        <button className="header__add-btn">+ Add Clothes</button>
+        {/* USERNAME */}
+        {/* USER AVATAR */}
+        <div className="header__user-container">
+            <p className="header__user-title">First Last</p>
+            <img src="" alt="" className="header__user-avatar" />
+        </div>
+       
+        </header>)
+}
+
+export default Header;
