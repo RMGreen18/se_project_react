48807b8cdc79030705e0b710f79d5e2895fe22d4
Bex Green <rebgreen18@gmail.com>
2025-02-19 18:31:37 -0500

    Create and style footer element

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+function Footer() {
+    return(
+        <div className="footer">
+            <p>Developed by Rebekah Green</p>
+            <p>2025</p>
+        </div>
+    )
+}
+
+export default Footer;
