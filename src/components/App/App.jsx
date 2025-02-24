116b8f246c9c1ac2c845791f9adaeb7484d3e1e9
Bex Green <rebgreen18@gmail.com>
2025-02-24 12:29:14 -0500

    fix: remove empty tags and correct radio buttons

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -39,79 +39,87 @@ function App() {
         const processedData = processWeatherData(data);
         setWeatherData(processedData);
       })
-      .catch((err) => {
-        console.error(err);
-      });
+      .catch(console.error);
   }, []);
 
   return (
-    <>
-      <div className="page">
-        <div className="page__content">
-          <Header handleAddClick={handleAddClick} weatherData={weatherData} />
-          <Main weatherData={weatherData} handleCardClick={handleCardClick} />
-          <Footer />
-        </div>
-        <ModalWithForm
-          title="New garment"
-          name="add-garment"
-          btnText="Add garment"
-          activeModal={activeModal}
-          onClose={closeActiveModal}
-        >
-          <label htmlFor="name" className="modal__label">
-            Name{" "}
-            <input
-              type="text"
-              className="modal__input"
-              id="name"
-              placeholder="Name"
-            />
-          </label>
-          <label htmlFor="imageUrl" className="modal__label">
-            Image{" "}
-            <input
-              type="url"
-              className="modal__input"
-              id="imageUrl"
-              placeholder="Image URL"
-            />
-          </label>
-          <label className="modal__label ">
-            <legend className="modal__legend">Set weather type:</legend>
-            <label
-              htmlFor="hot"
-              className="modal__label modal__label_type_radio"
-            >
-              <input id="hot" type="radio" className="modal__radio-input" />
-              {""} Hot
-            </label>
-            <label
-              htmlFor="warm"
-              className="modal__label modal__label_type_radio"
-            >
-              <input id="warm" type="radio" className="modal__radio-input" />
-              {""}
-              Warm
-            </label>
-            <label
-              htmlFor="cold"
-              className="modal__label modal__label_type_radio"
-            >
-              <input id="cold" type="radio" className="modal__radio-input" />
-              {""}
-              Cold
-            </label>
-          </label>
-        </ModalWithForm>
-        <ItemModal
-          name="image-preview"
-          activeModal={activeModal}
-          card={selectedCard}
-          onClose={closeActiveModal}
-        />
+    <div className="page">
+      <div className="page__content">
+        <Header handleAddClick={handleAddClick} weatherData={weatherData} />
+        <Main weatherData={weatherData} handleCardClick={handleCardClick} />
+        <Footer />
       </div>
-    </>
+      <ModalWithForm
+        title="New garment"
+        name="add-garment"
+        btnText="Add garment"
+        isOpen={activeModal === "add-garment"}
+        onClose={closeActiveModal}
+      >
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <label className="modal__label ">
+          <legend className="modal__legend">Set weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input
+              id="hot"
+              type="radio"
+              name="weatherType"
+              className="modal__radio-input"
+            />
+            {""} Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              id="warm"
+              type="radio"
+              name="weatherType"
+              className="modal__radio-input"
+            />
+            {""}
+            Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              id="cold"
+              type="radio"
+              name="weatherType"
+              className="modal__radio-input"
+            />
+            {""}
+            Cold
+          </label>
+        </label>
+      </ModalWithForm>
+      <ItemModal
+        name="image-preview"
+        isOpen={activeModal === "image-preview"}
+        card={selectedCard}
+        onClose={closeActiveModal}
+      />
+    </div>
   );
 }
 
