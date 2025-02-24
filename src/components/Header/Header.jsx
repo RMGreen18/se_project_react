a3250bb703ab562f02807054b7788ea4b023346a
Bex Green <rebgreen18@gmail.com>
2025-02-24 12:31:25 -0500

    fix: formatting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,31 @@
-import "./Header.css"
-import logo from "../../assets/logo.svg"
-import avatar from  "../../assets/avatar.png"
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
 
 function Header({ handleAddClick, weatherData }) {
-
-    const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric'});
-    return (<header className="header">
-        <img src={logo} alt="WTWR logo " className="header__logo" />
-        <p className="header__date-and-loc">{ currentDate }, {weatherData.city}</p>
-        <button onClick={handleAddClick} type="button" className="header__add-btn">+ Add Clothes</button>
-        <div className="header__user-container">
-            <p className="header__user-title">Terrence Tegegne</p>
-            <img src={avatar} alt="avatar" className="header__user-avatar" />
-        </div>
-       
-        </header>)
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <header className="header">
+      <img src={logo} alt="WTWR logo " className="header__logo" />
+      <p className="header__date-and-loc">
+        {currentDate}, {weatherData.city}
+      </p>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-btn"
+      >
+        + Add Clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__user-title">Terrence Tegegne</p>
+        <img src={avatar} alt="avatar" className="header__user-avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
