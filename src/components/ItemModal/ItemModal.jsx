f0abf15d6fad2a28514f9ae72828e11ffe3fd477
Bex Green <rebgreen18@gmail.com>
2025-02-24 12:34:13 -0500

    fix: edit class to follow BEM and make form universal by adding isOpen prop

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,20 +1,18 @@
 import "./ItemModal.css";
 
-function ItemModal({ name, activeModal, onClose, card }) {
-
-
+function ItemModal({ name, isOpen, onClose, card }) {
   return (
-    <div
-      className={`modal ${
-        activeModal === "image-preview" ? "modal__opened" : ""
-      } modal_type_${name}`}
-    >
+    <div className={`modal ${isOpen ? "modal_opened" : ""} modal_type_${name}`}>
       <div className="modal__content-image">
-        <button onClick={onClose} type="button" className="modal__close-image"></button>
+        <button
+          onClick={onClose}
+          type="button"
+          className="modal__close-image"
+        ></button>
         <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
-            <h2 className="modal__footer-title">{card.name}</h2>
-            <p className="modal__footer-info">Weather: {card.weather}</p>
+          <h2 className="modal__footer-title">{card.name}</h2>
+          <p className="modal__footer-info">Weather: {card.weather}</p>
         </div>
       </div>
     </div>
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,14 +1,10 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, title, btnText, name, activeModal, onClose }) {
+function ModalWithForm({ children, title, btnText, name, isOpen, onClose }) {
   return (
-    <div
-      className={`modal ${
-        activeModal === "add-garment" ? "modal__opened" : ""
-      } modal_type_${name}`}
-    >
+    <div className={`modal ${isOpen ? "modal_opened" : ""} modal_type_${name}`}>
       <div className="modal__content">
-        <button onClick={onClose} type="button" className="modal__close-form"></button>
+        <button onClick={onClose} type="button" className="modal__close-form" />
         <h2 className="modal__title">{title}</h2>
         <form className="modal__form" name={`${name}`}>
           {children}
