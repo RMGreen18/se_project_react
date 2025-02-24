5f9b13921afa3b4b9e2b6546c8b8c520c4683a2c
Bex Green <rebgreen18@gmail.com>
2025-02-24 12:32:16 -0500

    fix: formatting

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,14 +1,19 @@
-import './ItemCard.css'
+import "./ItemCard.css";
 function ItemCard({ item, onCardClick }) {
   const handleCardClick = () => {
     onCardClick(item);
   };
-    return(
-  <div className= "item__container">
-    <h2 className="item__name">{item.name}</h2>
-  <img onClick={handleCardClick} className="item__image" src={item.link} alt={item.name} />
-  </div>
-    );
+  return (
+    <div className="item__container">
+      <h2 className="item__name">{item.name}</h2>
+      <img
+        onClick={handleCardClick}
+        className="item__image"
+        src={item.link}
+        alt={item.name}
+      />
+    </div>
+  );
 }
 
 export default ItemCard;
